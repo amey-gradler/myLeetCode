46ecec758384d4a09a9906717432895fd631fd95
Ameya Ganore <62179297+amey-gradler@users.noreply.github.com>
2022-01-25 12:05:21 +0530

    Time: 50 ms (22.11%), Space: 22.4 MB (48.39%) - LeetHub

---
+++ 941-valid-mountain-array/941-valid-mountain-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool validMountainArray(vector<int>& arr) {
+        
+        int n =arr.size();
+        
+        int i=0;
+        int j=n-1;
+        
+        while(i+1<n && arr[i]<arr[i+1]) i++;
+        while(j>0 && arr[j-1]>arr[j]) j--;
+        
+        return i > 0 && i == j && j < n - 1;
+        
+    }
+};
