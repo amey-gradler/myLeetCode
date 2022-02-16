3e4e55ca1a1603e39f1a6bdc248cc011448987e0
Ameya Ganore <62179297+amey-gradler@users.noreply.github.com>
2022-02-16 18:06:09 +0530

    Time: 9 ms (18.13%), Space: 10.2 MB (72.50%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
@@ -6,24 +6,25 @@ public:
         
         int start=0;
         int end= nums.size()-1;
-        
-        while(start<end)
+        int n = nums.size();
+        while(start<=end)
         {
             int mid = start + (end-start)/2;
-            
-            if(mid>0 && nums[mid]< nums[mid-1])
+            int next = (mid+1)%n;
+            int prev = (mid+n-1)%n;
+            if(nums[mid]<=nums[prev] && nums[mid]<=nums[next])
                 return nums[mid];
-            else if(nums[mid]>=nums[start] && nums[mid]>nums[end])
+            else if(nums[mid]<=nums[end])
             {
-                start=mid+1;
+                end=mid-1;
             }
-            else{
-                end = mid-1;
+            else if(nums[start]<=nums[mid]){
+                start = mid+1;
             }
             
         }
         
-        return nums[start];
+        return -1;
         
     }
 };
