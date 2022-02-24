279621ed25994068e83a7ce2abb09ded647853fe
Ameya Ganore <62179297+amey-gradler@users.noreply.github.com>
2022-02-24 22:46:11 +0530

    Time: 72 ms (75.61%), Space: 48.5 MB (28.02%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -2,15 +2,12 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n=nums.size();
-        if(n==1) return true;
-        int step=nums[0];
+        int reachable=0;
         for(int i=0;i<n;i++)
         {
-            step--;
-            if(step<0)
-                return false;
-            if(nums[i]>step)
-                step=nums[i];
+            if(reachable<i) return false;
+            
+            reachable= max(reachable,i+nums[i]);
         }
         
         return true;
