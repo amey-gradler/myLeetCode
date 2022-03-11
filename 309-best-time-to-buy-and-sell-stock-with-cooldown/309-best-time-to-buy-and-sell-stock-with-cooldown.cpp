f031f2afb18a033552a98a8a0bd2f4f63b8471b0
Ameya Ganore <62179297+amey-gradler@users.noreply.github.com>
2022-03-11 17:50:17 +0530

    Time: 9 ms (37.00%), Space: 11.1 MB (83.89%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,38 +1,51 @@
 class Solution {
 public:
-    
-    int solve(int curr,vector<int> &prices,vector<int> &t)
-    {
-        int n=prices.size();
-        if(curr>=n)
-            return 0;
-        if(t[curr]!=-1)
-            return t[curr];
+    int maxProfit(vector<int>& arr) {
         
-        int maxVal=0;
-        for(int i=curr+1;i<n;i++)
+        int n= arr.size();
+        
+        int obsp=0-arr[0];
+        int ossp = 0;
+        int ocsp=0;
+        
+        for(int i=1;i<n;i++)
         {
-            //two choices
-            //1 - keep the stock in hand and move forward
-            //2 - sell the stock and calculate for i+2 pos
-            maxVal = max(maxVal, prices[i]-prices[curr] + solve(i+2,prices,t));
+            int nbsp=0;
+            int nssp=0;
+            int ncsp=0;
+            
+            if(ocsp-arr[i]>obsp)
+            {
+                nbsp = ocsp-arr[i];
+            }
+            else{
+                nbsp=obsp;
+            }
+            
+            if(obsp+arr[i]>ossp)
+            {
+                nssp = obsp+arr[i];
+            }
+            else
+            {
+                nssp = ossp;
+            }
+            
+            
+            if(ossp>ocsp)
+            {
+                ncsp = ossp;
+            }
+            else{
+                ncsp = ocsp;
+            }
+            
+            
+            obsp=nbsp;
+            ossp=nssp;
+            ocsp = ncsp;
+            
         }
-        
-        //or completly exclude the current element
-        maxVal = max(maxVal,solve(curr+1,prices,t));
-        t[curr]=maxVal;
-        return t[curr];
-        
-        
-    }
-    
-    
-    int maxProfit(vector<int>& prices) {
-        
-        int n=prices.size();
-        
-        vector<int> t(n+1,-1);
-        return solve(0,prices,t);
-        
+        return ossp;
     }
 };
