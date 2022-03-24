92a6637f33b912c079b58e5e3d7b8113add372a3
Ameya Ganore <62179297+amey-gradler@users.noreply.github.com>
2022-03-24 16:23:17 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (76.00%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -5,15 +5,39 @@ public:
         int n=nums.size();
         if(n==0)
             return 0;
-        vector<int> t(n+1);
-        t[0]=0;
-        t[1]=nums[0];
+//         tabulation logic
+        // vector<int> t(n+1);
+//         t[0]=0;
+//         t[1]=nums[0];
         
-        for(int i=2;i<=n;i++)
+//         for(int i=2;i<=n;i++)
+//         {
+//             t[i]=max(nums[i-1]+t[i-2] , t[i-1]);
+//         }
+//         return t[n];
+        
+        
+        //space optimized
+        // TC- O(n) SC - O(1)
+        
+        int prev=nums[0];
+        int prev2=0;
+        int curi=0;
+        for(int i=1;i<n;i++)
         {
-            t[i]=max(nums[i-1]+t[i-2] , t[i-1]);
+            int take= nums[i];
+            if(i>1) take+=prev2;
+            
+            int skip = 0+prev;
+            
+            curi = max(take,skip);
+            prev2=prev;
+            prev=curi;
+            
         }
-        return t[n];
+        return prev;
+        
+        
         
     }
 };
