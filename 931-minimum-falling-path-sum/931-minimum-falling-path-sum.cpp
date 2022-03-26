c4a88c8af133a2c40d4d3e6772a117aa01a37416
Ameya Ganore <62179297+amey-gradler@users.noreply.github.com>
2022-03-26 10:53:15 +0530

    Time: 26 ms (25.49%), Space: 10.2 MB (52.22%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,46 +1,80 @@
 class Solution {
 public:
     
-    int solve(int i,int j,vector<vector<int>> &m,vector<vector<int>> &dp)
-    {
-        if(i==m.size()-1)
-            return m[i][j];
+//     int solve(int i,int j,vector<vector<int>> &m,vector<vector<int>> &dp)
+//     {
+//         if(i==m.size()-1)
+//             return m[i][j];
         
-        if(dp[i][j]!=INT_MAX)
-            return dp[i][j];
+//         if(dp[i][j]!=INT_MAX)
+//             return dp[i][j];
         
-        int down=INT_MAX,ld=INT_MAX,rd=INT_MAX;
+//         int down=INT_MAX,ld=INT_MAX,rd=INT_MAX;
         
-        //down
-        down = m[i][j] + solve(i+1,j,m,dp);
+//         //down
+//         down = m[i][j] + solve(i+1,j,m,dp);
         
-        //ld
-        if(j>0)
-        {
-            ld = m[i][j]+solve(i+1,j-1,m,dp);
-        }
+//         //ld
+//         if(j>0)
+//         {
+//             ld = m[i][j]+solve(i+1,j-1,m,dp);
+//         }
         
-        if(j<m.size()-1)
-        {
-            rd = m[i][j] + solve(i+1,j+1,m,dp);
-        }
+//         if(j<m.size()-1)
+//         {
+//             rd = m[i][j] + solve(i+1,j+1,m,dp);
+//         }
         
-        return dp[i][j]=min(down,min(ld,rd));
+//         return dp[i][j]=min(down,min(ld,rd));
     
-    }
+//     }
     
     
-    int minFallingPathSum(vector<vector<int>>& matrix) {
+    int minFallingPathSum(vector<vector<int>>& m) {
         
-        int n = matrix.size();
+        int n = m.size();
         
         int ans=INT_MAX;
         
         vector<vector<int>> dp(n,vector<int>(n,INT_MAX));
         
+        // for(int j=0;j<n;j++)
+        // {
+        //     ans = min(ans,solve(0,j,matrix,dp));
+        // }
+        // return ans;
+        
+        //tabulation
+        
         for(int j=0;j<n;j++)
         {
-            ans = min(ans,solve(0,j,matrix,dp));
+            dp[n-1][j] = m[n-1][j]; 
+        }
+        
+        
+        for(int i=n-2;i>=0;i--)
+        {
+            for(int j=n-1;j>=0;j--)
+            {
+                int down=INT_MAX,ld=INT_MAX,rd=INT_MAX;
+                
+                //down;
+                down =  dp[i+1][j];
+                //ld
+                if(j>0)
+                    ld= dp[i+1][j-1];
+                //rd
+                if(j<n-1)
+                    rd = dp[i+1][j+1];
+                
+                dp[i][j] = m[i][j] + min(down,min(ld,rd));
+                
+            }
+        }
+        
+        for(int j=0;j<n;j++)
+        {
+            ans=min(ans,dp[0][j]);
         }
         return ans;
         
