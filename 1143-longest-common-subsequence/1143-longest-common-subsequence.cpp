9d8bb6a28924dbec38a691bdac99d6aaa9b56a19
Ameya Ganore <62179297+amey-gradler@users.noreply.github.com>
2022-03-29 19:39:28 +0530

    Time: 43 ms (25.31%), Space: 12.9 MB (48.41%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -26,7 +26,33 @@ public:
         
         vector<vector<int>> dp(n+1,vector<int> (m+1,-1));
         
-        return solve(n,m,text1,text2,dp);
+        for(int i=0;i<n+1;i++)
+        {
+            dp[i][0] = 0;
+        }
+        
+        for(int j=0;j<m+1;j++)
+        {
+            dp[0][j]=0;
+        }
+        
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=1;j<m+1;j++)
+            {
+                
+                //matching
+                if(text1[i-1]==text2[j-1])
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else
+                    dp[i][j] = max( dp[i-1][j] , dp[i][j-1] );
+                
+            }
+        }
+        
+        return dp[n][m];
+        
+        // return solve(n,m,text1,text2,dp);
                 
         
     }
