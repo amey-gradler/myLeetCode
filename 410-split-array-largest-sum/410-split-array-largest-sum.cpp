1c12d42d1f400ee25888a0cf40a4523e51787003
Ameya Ganore <62179297+amey-gradler@users.noreply.github.com>
2022-03-31 19:11:24 +0530

    Time: 6 ms (39.54%), Space: 7.2 MB (74.76%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -1,45 +1,48 @@
 class Solution {
 public:
-    int find(int m, int s, int e, int n,vector<int>&csum,int dp[51][1001])
+    
+    bool isValid(vector<int> &nums , int n,int k,int mid)
     {
-        if(m == 1)
+        int s = 1;
+        int sum=0;
+        for(int i=0;i<n;i++)
         {
-            return dp[m][e] = csum[n-1] - csum[s];
+            sum+=nums[i];
+            if(sum>mid)
+            {
+                s++;
+                sum=nums[i];
+            }
+            if(s>k) return false;
         }
-        if(e == n-1)
-        {
-            return dp[m][e] = csum[e];
-        }
-        if(dp[m][e] != -1)
-            return dp[m][e];
-        int finalAns = INT_MAX;
-        for(int i = e;i<n-1;i++)
-        {
-            int sum = csum[i]-csum[s];
-            int t =  find(m-1,i,i+1,n,csum,dp);
-            
-            int ans =max(sum,t);
-            finalAns = min(finalAns,ans);
-        }
-        return dp[m][e] = finalAns;
+        return true;
+        
     }
+    
+    
     int splitArray(vector<int>& nums, int m) {
-        vector<int>csum ;
-        csum.push_back(0);
-        csum.push_back(nums[0]);
-        int sum = nums[0];
-        if(nums.size() == 1)
-            return nums[0];
-        int dp[51][1001];
-        memset(dp,-1,sizeof(dp));
-        for(int i = 1;i<nums.size();i++)
+        
+        int n = nums.size();
+        
+        int start = *max_element(nums.begin(),nums.end());
+        int end = accumulate(nums.begin(),nums.end(),0);
+        
+        int res;
+        while(start<=end)
         {
+            int mid = start + (end-start)/2;
+                
+            if(isValid(nums,n,m,mid))
+            {
+                res=mid;
+                end = mid-1;
+            }
+            else
+                start=mid+1;
             
-            sum+= nums[i];
-            csum.push_back(sum);
         }
-        int n = csum.size();
-        int  ans = find(m,0,1,n,csum,dp);
-        return ans;
+        
+        return res;
+        
     }
 };
