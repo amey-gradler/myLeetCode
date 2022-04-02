e5464a90428ffa27e8cc5dcfcb83abf2007ecead
Ameya Ganore <62179297+amey-gradler@users.noreply.github.com>
2022-04-02 10:26:14 +0530

    Time: 118 ms (5.04%), Space: 30 MB (24.26%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -27,10 +27,40 @@ public:
         int n = s.length();
         int m = t.length();
         
-        vector<vector<int>> dp(n,vector<int>(m,-1));
+        // vector<vector<int>> dp(n,vector<int>(m,-1));
         
-        return f(n-1,m-1,s,t,dp);
+        // return f(n-1,m-1,s,t,dp);
         
+        vector<vector<double>> dp(n+1,vector<double>(m+1,0));
+        
+        for(int i=0;i<n+1;i++)
+        {
+            for(int j=0;j<m+1;j++)
+            {
+                if(i==0)
+                    dp[i][j] = 0;
+                if(j==0)
+                    dp[i][j]=1;
+            }
+        }
+        
+        
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=1;j<m+1;j++)
+            {
+                if(s[i-1]==t[j-1])
+                {
+                    dp[i][j] = dp[i-1][j-1]+dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }    
+            
+            }
+        }
+        
+        return (int)dp[n][m];        
         
     }
 };
