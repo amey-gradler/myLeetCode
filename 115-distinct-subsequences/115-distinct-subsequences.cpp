421ce9a1f81cca2f14b5a41c7816dfa550943192
Ameya Ganore <62179297+amey-gradler@users.noreply.github.com>
2022-04-02 10:30:47 +0530

    Time: 24 ms (87.50%), Space: 6.4 MB (91.82%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -31,36 +31,57 @@ public:
         
         // return f(n-1,m-1,s,t,dp);
         
-        vector<vector<double>> dp(n+1,vector<double>(m+1,0));
+        // vector<vector<double>> dp(n+1,vector<double>(m+1,0));
         
-        for(int i=0;i<n+1;i++)
-        {
-            for(int j=0;j<m+1;j++)
-            {
-                if(i==0)
-                    dp[i][j] = 0;
-                if(j==0)
-                    dp[i][j]=1;
-            }
-        }
+//         for(int i=0;i<n+1;i++)
+//         {
+//             for(int j=0;j<m+1;j++)
+//             {
+//                 if(i==0)
+//                     dp[i][j] = 0;
+//                 if(j==0)
+//                     dp[i][j]=1;
+//             }
+//         }
         
         
-        for(int i=1;i<n+1;i++)
-        {
-            for(int j=1;j<m+1;j++)
-            {
-                if(s[i-1]==t[j-1])
-                {
-                    dp[i][j] = dp[i-1][j-1]+dp[i-1][j];
-                }
-                else{
-                    dp[i][j] = dp[i-1][j];
-                }    
+//         for(int i=1;i<n+1;i++)
+//         {
+//             for(int j=1;j<m+1;j++)
+//             {
+//                 if(s[i-1]==t[j-1])
+//                 {
+//                     dp[i][j] = dp[i-1][j-1]+dp[i-1][j];
+//                 }
+//                 else{
+//                     dp[i][j] = dp[i-1][j];
+//                 }    
             
-            }
-        }
+//             }
+//         }
         
-        return (int)dp[n][m];        
+//         return (int)dp[n][m];        
+        
+            vector<double> prev(m+1,0) , curr(m+1,0);
+            prev[0]=curr[0] = 1;
+        
+            for(int i=1;i<n+1;i++)
+            {
+                for(int j=1;j<m+1;j++)
+                {
+                    if(s[i-1]==t[j-1])
+                    {
+                        curr[j] = prev[j-1]+prev[j];
+                    }
+                    else{
+                        curr[j] = prev[j];
+                    }    
+
+                }
+                prev=curr;
+            }
+        
+            return (int)prev[m];    
         
     }
 };
