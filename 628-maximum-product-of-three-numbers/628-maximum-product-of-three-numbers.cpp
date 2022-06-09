f49dca06a067029cfd4b45cb47248d7808453daf
Ameya Ganore <62179297+amey-gradler@users.noreply.github.com>
2022-06-09 11:26:11 +0530

    Time: 83 ms (14.54%), Space: 27.8 MB (36.52%) - LeetHub

--- 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.cpp
+++ 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.cpp
@@ -3,9 +3,42 @@ public:
     int maximumProduct(vector<int>& nums) {
         
         
-        sort(nums.begin(),nums.end());
+        // sort(nums.begin(),nums.end());
         int n = nums.size();
-        return max(nums[n-1]*nums[n-2]*nums[n-3] , nums[0]*nums[1]*nums[n-1]);
+        // return max(nums[n-1]*nums[n-2]*nums[n-3] , nums[0]*nums[1]*nums[n-1]);
+        
+        int max1=INT_MIN,max2=INT_MIN,max3=INT_MIN,min1=INT_MAX,min2=INT_MAX;
+        
+        for(int i :  nums)
+        {
+            if(i>max1)
+            {
+                max3=max2;
+                max2=max1;
+                max1=i;
+            }
+            else if(i>max2)
+            {
+                max3=max2;
+                max2=i;
+            }
+            else if(i>max3)
+            {
+                max3=i;
+            }
+            
+            if(i<min1)
+            {
+                min2=min1;
+                min1=i;
+            }
+            else if(i<min2)
+            {
+                min2=i;
+            }
+        }        
+        
+        return max(max1*max2*max3,max1*min1*min2);
         
     }
 };
