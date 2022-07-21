08db1c7040f1469918a5acfd69ee115f04592a27
Ameya Ganore <62179297+amey-gradler@users.noreply.github.com>
2022-07-21 09:40:06 +0530

    Time: 783 ms (5.00%), Space: 399.9 MB (5.40%) - LeetHub

---
+++ 72-edit-distance/72-edit-distance.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    
+    int solve(string s1,string s2, int i,int j,vector<vector<int>>& dp)
+    {
+        //base case
+        //s1 is exhausted
+        if(i<0)
+            return j+1;
+        
+        //s2 exhausted
+        if(j<0)
+            return i+1;
+            
+        if(dp[i][j]!=INT_MAX) return dp[i][j];
+        //rec
+        
+        if(s1[i]==s2[j]) return 0+solve(s1,s2,i-1,j-1,dp);
+        
+        int ins = 1 + solve(s1,s2,i,j-1,dp);
+        int del = 1 + solve(s1,s2,i-1,j,dp);
+        int rep = 1 + solve(s1,s2,i-1,j-1,dp);
+        
+        return dp[i][j] = min(ins,min(del,rep));
+        
+    }
+    
+    int minDistance(string word1, string word2) {
+        
+        int n = word1.size();
+        int m = word2.size();
+        
+        vector<vector<int>> dp(n,vector<int>(m,INT_MAX));
+        return solve(word1,word2,n-1,m-1,dp);
+        
+    }
+};
