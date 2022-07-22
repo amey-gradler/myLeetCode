205e4d4a4c5a3343cdcb6e4f76c8fefda5f5d99a
Ameya Ganore <62179297+amey-gradler@users.noreply.github.com>
2022-07-22 11:01:23 +0530

    Time: 272 ms (48.12%), Space: 23.8 MB (51.76%) - LeetHub

--- 935-knight-dialer/935-knight-dialer.cpp
+++ 935-knight-dialer/935-knight-dialer.cpp
@@ -3,23 +3,23 @@
 class Solution {
 public:
     
-//     int solve(int i,int n,unordered_map<int,vector<int>> &mp , vector<vector<int>> &t)
-//     {
-//         if(n==1)
-//             return 1;
+    int solve(int i,int n,unordered_map<int,vector<int>> &mp , vector<vector<int>> &t)
+    {
+        if(n==1)
+            return 1;
         
-//         if(t[i][n]!=-1)
-//             return t[i][n];
+        if(t[i][n]!=-1)
+            return t[i][n];
         
-//         int ans=0;
-//         for(auto it : mp[i])
-//         {
-//             t[it][n-1] = solve(it,n-1,mp,t) ;
-//             ans= (ans + t[it][n-1])%MOD;
-//         }
+        int ans=0;
+        for(auto it : mp[i])
+        {
+            t[it][n-1] = solve(it,n-1,mp,t) ;
+            ans= (ans + t[it][n-1])%MOD;
+        }
         
-//         return ans;
-//     }
+        return ans;
+    }
     
     
     int knightDialer(int n) {
@@ -35,36 +35,43 @@ public:
         mp[8] = {1,3};
         mp[9] = {2,4};
         
-        int t[10][n+1];
+        // int t[10][n+1];
         
-        for(int i=0;i<10;i++)
-        {
-            t[i][1]=1;
-        }
+//         for(int i=0;i<10;i++)
+//         {
+//             t[i][1]=1;
+//         }
         
-        for(int j=2;j<n+1;j++)
-        {   
-            for(int i=0;i<10;i++)
-            {
-                vector<int> v;
-                v = mp[i];
-                int ans=0;
-                for(auto it : v)
-                {
-                    ans = (ans + t[it][j-1])%MOD;
-                }
-                t[i][j] = ans;
-                ans=0;
-            }
-        }
+//         for(int j=2;j<n+1;j++)
+//         {   
+//             for(int i=0;i<10;i++)
+//             {
+//                 vector<int> v;
+//                 v = mp[i];
+//                 int ans=0;
+//                 for(auto it : v)
+//                 {
+//                     ans = (ans + t[it][j-1])%MOD;
+//                 }
+//                 t[i][j] = ans;
+//                 ans=0;
+//             }
+//         }
+        
+//         int ans=0;
+//         for(int i= 0;i<10;i++)
+//         {
+//             ans = (ans + t[i][n])%MOD;
+//             cout<<t[i][n]<<endl;
+//         }
         
         int ans=0;
+        vector<vector<int>> t(10,vector<int>(n+1,-1));
         for(int i= 0;i<10;i++)
         {
-            ans = (ans + t[i][n])%MOD;
-            cout<<t[i][n]<<endl;
+            ans = (ans + solve(i,n,mp,t))%MOD;
+            // cout<<t[i][n]<<endl;
         }
-        
         
         return ans;
         
