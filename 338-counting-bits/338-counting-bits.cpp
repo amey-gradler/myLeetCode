5c18af398d83eb3ddd10c397a766ce0b305e99ce
Ameya Ganore <62179297+amey-gradler@users.noreply.github.com>
2022-07-22 10:44:24 +0530

    Time: 10 ms (44.91%), Space: 7.8 MB (87.75%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -26,11 +26,25 @@ public:
         vector<int> ans(n+1,0);
         if(n==0)
             return ans;
+        // ans[1]=1;
+        // for(int i=0;i<=n;i++)
+        // {
+        //         ans[i]= (solve(i,ans));
+        // }
+        
+        ans[0]=0;
         ans[1]=1;
-        for(int i=0;i<=n;i++)
+        for(int i=2;i<=n;i++)
         {
-                ans[i]= (solve(i,ans));
+            if(i%2==0)
+            {
+                ans[i]=ans[i/2];
+            }
+            else{
+                ans[i] = 1 + ans[i/2];
+            }
         }
+        
         return ans;
     }
 };
