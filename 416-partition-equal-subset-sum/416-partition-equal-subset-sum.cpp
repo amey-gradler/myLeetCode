9183defcc60786a9fcbfd4e4af99867e020cc35a
Ameya Ganore <62179297+amey-gradler@users.noreply.github.com>
2022-07-28 20:47:26 +0530

    Time: 89 ms (90.53%), Space: 75.4 MB (22.14%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,44 +1,39 @@
 class Solution {
 public:
+    
+    bool solve(int i,vector<int>&nums,int t,vector<vector<int>>&dp)
+    {
+        if(t==0)
+            return true;
+        
+        if(i==0)
+            return nums[i]==t;
+        
+        if(dp[i][t]!=-1)
+            return dp[i][t];
+        
+        if(nums[i]<=t)
+        {
+            return dp[i][t] = solve(i-1,nums,t-nums[i],dp) || solve(i-1,nums,t,dp);
+        }
+        else
+            return dp[i][t]= solve(i-1,nums,t,dp);
+        return false;
+        
+    }
+    
+    
     bool canPartition(vector<int>& nums) {
         
         int sum=0;
-        for(int i : nums)
+        for(int i: nums)
             sum+=i;
-        if(sum&1)
+        if(sum%2!=0)
             return false;
         
-        sum=sum>>1; //sum/=2
-        int n=nums.size();
-        //Initialisation
-        int t[n+1][sum+1];
-        
-        for(int i=0;i<n+1;i++)
-        {
-            for(int j=0;j<sum+1;j++)
-            {
-                if(i==0)
-                    t[i][j]=false;
-                if(j==0)
-                    t[i][j]=true;
-            }
-        }
-        
-        for(int i=1;i<n+1;i++)
-        {
-            for(int j=1;j<sum+1;j++)
-            {
-                if(nums[i-1]<=j)
-                {
-                    t[i][j]= t[i-1][j] || t[i-1][j-nums[i-1]];
-                }
-                else{
-                    t[i][j]=t[i-1][j];
-                }
-            }
-        }
-        return t[n][sum];
-        
-        
+        sum=sum/2;
+        int n = nums.size();
+        vector<vector<int>> dp(n,vector<int>(sum+1,-1));
+        return solve(n-1,nums,sum,dp);
     }
 };
