bd79bed9eb2de52208bfc84041e41f5b1e6dca7c
Ameya Ganore <62179297+amey-gradler@users.noreply.github.com>
2022-08-14 16:15:28 +0530

    Time: 5 ms (18.62%), Space: 6.5 MB (46.25%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int solve(int i,int j,int m,int n,vector<vector<int>> &dp)
+    {
+        if(i==0 && j==0)
+            return 1;
+        
+        if(i<0 || i>=m || j<0 || j==n)
+            return 0;
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        int left = solve(i,j-1,m,n,dp);
+        int up = solve(i-1,j,m,n,dp);
+        
+        return  dp[i][j]=left+up; 
+        
+    }
+    
+    int uniquePaths(int m, int n) {
+        
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return solve(m-1,n-1,m,n,dp);
+        
+    }
+};
